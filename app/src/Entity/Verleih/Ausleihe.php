0392a2d414c623dc64fe738540ec602fa9ab835d
Marcus Häußler <marcus.haeussler@lidl.com>
2019-05-09 12:19:44 +0200

    Verleih Domäne hinzugefügt

---
+++ app/src/Entity/Verleih/Ausleihe.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Entity\Verleih;
+
+/**
+ * @author Marcus Häußler <marcus.haeussler@lidl.com>
+ */
+final class Ausleihe
+{
+    private $userId;
+    private $buchId;
+    private $von;
+    private $bis;
+    private $abgeschlossen = false;
+    /**
+     * @var \DateTimeInterface
+     */
+    private $abgeschlossenAm;
+
+    private function __construct(string $userId, string $buchId, \DateTimeInterface $von, \DateTimeInterface $bis)
+    {
+        $this->userId = $userId;
+        $this->buchId = $buchId;
+        $this->von = $von;
+        $this->bis = $bis;
+    }
+
+    public static function fuerNutzer(string $userId, string $buchId, \DateTimeInterface $von, \DateTimeInterface $bis): self
+    {
+        return new self($userId, $buchId, $von, $bis);
+    }
+
+    public function abschliessen(): void
+    {
+        $this->abgeschlossen = true;
+        $this->abgeschlossenAm = new \DateTimeImmutable();
+    }
+
+    public function abgeschlossen(): bool
+    {
+        return $this->abgeschlossen;
+    }
+
+    public function buchId(): string
+    {
+        return $this->buchId;
+    }
+}
---
+++ app/src/Entity/Verleih/Buch.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Entity\Verleih;
+
+/**
+ * @author Marcus Häußler <marcus.haeussler@lidl.com>
+ */
+final class Buch
+{
+    private $id;
+    private $titel;
+    /**
+     * @var Ausleihe[]
+     */
+    private $ausleihVerlauf = [];
+
+    public function __construct(string $id, string $titel)
+    {
+        $this->id = $id;
+        $this->titel = $titel;
+    }
+
+    public function ausleihen(string $nutzerId, \DateTimeInterface $von, \DateTimeInterface $bis): void
+    {
+        foreach ($this->ausleihVerlauf as $ausleihe) {
+            if (!$ausleihe->abgeschlossen()) {
+                throw new \DomainException(sprintf('Buch %s wurde noch nicht zurückgegeben', $this->titel()));
+            }
+        }
+
+        $this->ausleihVerlauf[] = Ausleihe::fuerNutzer($nutzerId, $this->id, $von, $bis);
+    }
+
+    public function zurueckgeben(): void
+    {
+        foreach ($this->ausleihVerlauf as $ausleihe) {
+            if (!$ausleihe->abgeschlossen()) {
+                $ausleihe->abschliessen();
+            }
+        }
+    }
+
+    public function titel(): string
+    {
+        return $this->titel;
+    }
+}
